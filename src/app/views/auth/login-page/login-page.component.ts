ac3770f42757e2f21b9f7ceab9d9ff9bf54ace85
ITheCorgi <xmavrik74@gmail.com>
2020-12-09 15:38:38 +0500

    daily changes

--- src/app/views/auth/login-page/login-page.component.ts
+++ src/app/views/auth/login-page/login-page.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
   
   loginForm: FormGroup;
-  //email = new FormControl('', [Validators.required, Validators.email]);
-  submitted = false;
-  error = '';
- 
+
   /**
      * Constructor
      * @param private formBuilder: FormBuilder,
      */
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+  constructor(private _formBuilder: FormBuilder) {
+    this.loginForm = this._formBuilder.group({
+      email   : ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
