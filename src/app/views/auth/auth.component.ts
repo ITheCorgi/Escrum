35bac02f049c29e7aa0918a7b59d250e55a68af7
Max Gustov <maxim.gustov@emerson.com>
2021-02-18 00:09:15 +0500

    auth

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { AuthService } from 'src/app/@core/services/auth.service'
+import { IUser } from '../../@core/models/user'
+import { Role } from '../../@core/models/role'
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
+  user!: IUser;
 
   constructor(private authService: AuthService) { 
-    
+    this.authService.user$.subscribe(x => this.user = x);
   }
 
-  ngOnInit(): void {
+  isSuperAdmin(): boolean {
+    return this.user && this.user === Role.SuperAdmin;
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 
 }
