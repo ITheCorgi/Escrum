c75da2edce75aa280ede3bde8bfc7201a084d5e8
Max Gustov <maxim.gustov@emerson.com>
2021-03-01 13:13:51 +0500

    draft signup

--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AuthService } from '../../../@core/services/auth.service'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  signupForm!: FormGroup;
+  hide!: boolean;
+  private _submitted!: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _authService: AuthService
+  ) { 
+    //this._submitted = false;
   }
 
+  ngOnInit(): void {
+    this.signupForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  public get submitted() {
+    return this._submitted;
+  }
+
+  public set submitted(value: boolean) {
+    this._submitted = value;
+  }
+
+  public get email() {
+    return this.signupForm.controls.email;
+  }
+
+  public get name() {
+    return this.signupForm.controls.name;
+  }
+
+  public get password() {
+    return this.signupForm.controls.password;
+  }
 }
