70b0fb282788abab98fe2e3cae42401561505861
Max Gustov <maxim.gustov@emerson.com>
2021-03-09 21:15:15 +0500

    minor

--- src/app/views/auth/login-page/login-page.component.ts
+++ src/app/views/auth/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faMicrosoft, faGithub, faGoogle, faFacebook, faVk } from '@fortawesome/free-brands-svg-icons';
 import { Router, ActivatedRoute } from '@angular/router'
 import { first } from 'rxjs/operators';
